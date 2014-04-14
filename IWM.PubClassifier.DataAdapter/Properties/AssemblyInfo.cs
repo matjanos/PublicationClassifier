3a0a23e279f29c35765e2be254c895e0879cc551
Kuba Matjanowski <k.matjanowski@gmail.com>
2014-04-14 18:35:36 +0200

    First Commit. Documents, Projects and solution are created and ready to include libraries and create classes.

---
+++ IWM.PubClassifier.DataAdapter/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Ogólne informacje o zestawie są kontrolowane poprzez następujący 
+// zbiór atrybutów. Zmień wartości tych atrybutów by zmodyfikować informacje
+// powiązane z zestawem.
+[assembly: AssemblyTitle("IWM.PubClassifier.DataAdapter")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("IWM.PubClassifier.DataAdapter")]
+[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Ustawienie wartości ComVisible na false sprawia, że typy w tym zestawie nie będą widoczne 
+// dla składników COM.  Jeśli potrzebny jest dostęp do typu w tym zestawie z 
+// COM, ustaw wartość ComVisible na true, dla danego typu.
+[assembly: ComVisible(false)]
+
+// Następujący GUID jest dla ID typelib jeśli ten projekt jest dostępny dla COM
+[assembly: Guid("5b6b2273-c1b5-49d1-8783-e316b70c1bb8")]
+
+// Informacje o wersji zestawu zawierają następujące cztery wartości:
+//
+//      Wersja główna
+//      Wersja pomocnicza 
+//      Numer kompilacji
+//      Rewizja
+//
+// Można określać wszystkie wartości lub używać domyślnych numerów kompilacji i poprawki 
+// poprzez użycie '*', jak pokazane jest poniżej:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
